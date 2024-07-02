b4127ea89a8c2d5605f9942d0b4a6ca5de79c9ab
Mazo667 <maxifava667@gmail.com>
2024-07-01 23:07:20 -0300

    Tp Final finalizado

--- tp3.c
+++ tp3.c
@@ -5,6 +5,7 @@
 
 /* libreria de Hilos */
 #include <pthread.h>
+#include <unistd.h>
 
 /* Libreria de señales */
 #include <signal.h>
@@ -34,56 +35,16 @@
 int arp_count = 0, icmp_count = 0, ip_count = 0, udp_count = 0, tcp_count = 0;
 pthread_mutex_t count_mutex;
 
-/* Cabecera ethernet */
-struct sniff_ethernet {
-        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
-        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
-        u_short ether_type;                     /* IP? ARP? RARP? etc */
-};
-
-/* Cabecera IP */
-struct sniff_ip {
-        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
-        u_char  ip_tos;                 /* type of service */
-        u_short ip_len;                 /* total length */
-        u_short ip_id;                  /* identification */
-        u_short ip_off;                 /* fragment offset field */
-        #define IP_RF 0x8000            /* reserved fragment flag */
-        #define IP_DF 0x4000            /* don't fragment flag */
-        #define IP_MF 0x2000            /* more fragments flag */
-        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
-        u_char  ip_ttl;                 /* time to live */
-        u_char  ip_p;                   /* protocol */
-        u_short ip_sum;                 /* checksum */
-        struct  in_addr ip_src,ip_dst;  /* source and dest address */
-};
-#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
-#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
-
-/* Cabecera TCP */
-typedef u_int tcp_seq;
-
-struct sniff_tcp {
-        u_short th_sport;               /* source port */
-        u_short th_dport;               /* destination port */
-        tcp_seq th_seq;                 /* sequence number */
-        tcp_seq th_ack;                 /* acknowledgement number */
-        u_char  th_offx2;               /* data offset, rsvd */
-#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
-        u_char  th_flags;
-        #define TH_FIN  0x01
-        #define TH_SYN  0x02
-        #define TH_RST  0x04
-        #define TH_PUSH 0x08
-        #define TH_ACK  0x10
-        #define TH_URG  0x20
-        #define TH_ECE  0x40
-        #define TH_CWR  0x80
-        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-        u_short th_win;                 /* window */
-        u_short th_sum;                 /* checksum */
-        u_short th_urp;                 /* urgent pointer */
-};
+// Función que se ejecutará cada vez que se capture una trama en tiempo real
+void *print_stats(void *arg) {
+    while (1) {
+        sleep(1);
+        pthread_mutex_lock(&count_mutex);
+        printf("ARP: %d, ICMP: %d, IP: %d, UDP: %d, TCP: %d\n", arp_count, icmp_count, ip_count, udp_count, tcp_count);
+        pthread_mutex_unlock(&count_mutex);
+    }
+    return NULL;
+}
 
 // Función que se ejecutará cada vez que se capture una trama
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
@@ -206,6 +167,9 @@ int main(int argc, char* argv[]){
     if(argc != 2){
         printf("Para ejecutar correctamente ingrese: <numero de tramas a capturar>\n");
         return 1;
+    } else if (atoi(argv[1]) > 10 || atoi(argv[1]) < 0){
+        printf("El numero de tramas a capturar debe ser un numero positivo menor o igual a 10\n");
+        return 1;
     }
 
     // Asignar el numero de tramas a capturar
@@ -283,26 +247,23 @@ int main(int argc, char* argv[]){
 
     signal(SIGINT, sig_int); // Manejador de señales
 
-
-
     // Verifica si el parametro es 0, si es 0 se ejecutara indefinidamente si no se ejecutara el numero de tramas ingresado
     if(strlen(argv[1]) == 0){
         pcap_loop(handle, -1 , got_packet, NULL);
-    }else{
+    }else if (strlen(argv[1]) <= 10 || strlen(argv[1]) >= 1){
         pcap_loop(handle, num_packets, got_packet, NULL);
     }
 
-	/* cleanup */
-	pcap_freecode(&fp);
-	pcap_close(handle);
-
-	printf("Datos Estadisticos Finales: \n");
+	printf("\nDatos Estadisticos Finales: \n");
     printf("Tramas capturadas de tipo ARP: %d\n", arp_count);
     printf("Tramas capturadas de tipo ICMP: %d\n", icmp_count);
     printf("Tramas capturadas de tipo IP: %d\n", ip_count);
     printf("Tramas capturadas de tipo UDP: %d\n", udp_count);
     printf("Tramas capturadas de tipo TCP: %d\n", tcp_count);
     
+    pcap_close(handle); // Cierra el descriptor de la interfaz
+
+    pthread_mutex_destroy(&count_mutex); // Destruye el mutex
 
     return 0;
 }
